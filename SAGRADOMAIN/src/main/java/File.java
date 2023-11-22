fa286b530cee4f95af07d59ffb4dbe3555c4a1ac
Dingpals <150411456+Dingpals@users.noreply.github.com>
2023-11-23 05:08:56 +0800

    Add files via upload

---
+++ SAGRADOMAIN/src/main/java/File.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+class File {
+
+    File(String filePath) {
+    }
+
+    boolean exists() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   supportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
---
+++ SAGRADOMAIN/src/main/java/com/mycompany/sagradomain/SAGRADOMAIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sagradomain;
+
+/**
+ *
+ * @author USER
+ */
+public class SAGRADOMAIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
